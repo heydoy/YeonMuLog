8b9a5143d813713dc43d97ba9dafedfde94ddde6
Doy Kim <kimdee.dev@gmail.com>
2022-09-13 22:00:57 +0900

    Initial Commit

---
+++ YeonMuLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YeonMuLog
+//
+//  Created by Doy Kim on 2022/09/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

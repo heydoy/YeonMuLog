538ffe57929bed2328dbfdac7cb8770a1c624c4a
Doy Kim <kimdee.dev@gmail.com>
2022-09-18 20:43:07 +0900

    [Feature] - #2 KOPIS API매니저에서 공연목록 조회 메서드 선언, ATS 세팅

--- YeonMuLog/Data/Network/KOPISAPIManager.swift
+++ YeonMuLog/Data/Network/KOPISAPIManager.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import Alamofire
+
+class KOPISAPIManager {
+    private init() {}
+    static let shared = KOPISAPIManager()
+    // 공연목록 조회
+    func callList(query: String) {
+        
+        let url = EndPoints.KOPISList
+        
+        let param: Parameters = [
+            "service": APIKeys.KOPIS, // 서비스키
+            "shprfnm": query, // 쿼리
+            "rows": 100, // 받아오는 개수
+            "cpage": 1 // 시작하는 숫자
+        ]
+        AF.request(url,
+                   method: .get,
+                   parameters: param).validate().responseData { response in
+            switch response.result {
+            case .success(let value):
+                print(value)
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+}

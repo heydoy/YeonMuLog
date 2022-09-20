7f03b021de006c487cd49159ea5a2a05e65c6f77
Doy Kim <kimdee.dev@gmail.com>
2022-09-20 21:47:44 +0900

    [Feature] - #1 Realm에 저장할 모델 클래스 프로퍼티 추가 및 변경 - 날짜는 Date 타입으로 - 캐스팅 위치 변경 - _id를 id로 네이밍바꿈

--- YeonMuLog/Data/Repository/UserPlayModel.swift
+++ YeonMuLog/Data/Repository/UserPlayModel.swift
@@ -10,24 +10,26 @@ import RealmSwift
 import UIKit
 
 class UserPlayInfo: Object {
-    @Persisted(primaryKey: true) var _id: ObjectId // ID값
+    @Persisted(primaryKey: true) var id: ObjectId // ID값
     @Persisted var playId: String //KOPIS에서 제공할때 쓰는 공연 고유 ID
     @Persisted var title: String // 공연제목
-    @Persisted var cast: List<String> // 공연캐스트 (Play모델 안에서 ,로 구분해서 사용자에게 보여주고 선택하게 하기)
+    
     @Persisted var genre: String // 장르
     @Persisted var poster: String // 포스터이미지 웹링크
     @Persisted var place: String // 공연장소
-    @Persisted var date: String // 관람한 공연날짜
+    @Persisted var date: Date // 관람한 공연날짜
     @Persisted var runtime: String // 공연런타임
     @Persisted var seat: String // 자리
     @Persisted var ticket: Int // 티켓종류 가격 (R석 52,000원, S석 44,000원 식. 사용자가 재관할인등 할인을 받을 수 있으므로 일단 저장만 해두고 나중에 디테일하게 가격을 받을 수 있도록 해야되겠다. 입력받을 때는 무조건 Int로 받게끔)
     @Persisted var time: String // 공연시간
     @Persisted var userReview: List<UserReview> // 사용자 리뷰 모음
+    @Persisted var casts: List<String> // 공연캐스트 (Play모델 안에서 ,로 구분해서 사용자에게 보여주고 선택하게 하기)
 }
 
 class UserReview: Object {
-    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted(primaryKey: true) var id: ObjectId
     @Persisted var text: String
     @Persisted var image: List<String> // 도큐먼트에 저장한 이미지 URL
     
 }
+

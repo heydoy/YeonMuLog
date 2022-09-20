1c046a39bddf4e8c178afbf9cfb1f1ea753f1f9b
Doy Kim <kimdee.dev@gmail.com>
2022-09-20 21:49:36 +0900

    [Text] - feature/#1에서 모델 저장 및 fetch를 위한 테스트코드 작성

--- YeonMuLog/Presentations/ViewController.swift
+++ YeonMuLog/Presentations/ViewController.swift
@@ -32,6 +32,40 @@ class ViewController: UIViewController {
                 print(error.localizedDescription)
             }
         }
-  
+        
+        print("####-----------------------")
+        let list = UserPlayInfo()
+        list.date = Date()
+        list.casts.append(objectsIn: ["소영", "윤소영", "지율"])
+
+        list.playId = "PF334482"
+        list.genre = "연극"
+        list.seat = "S12"
+        list.ticket = 66000
+        list.poster = ""
+        list.title = "닥터지바고"
+        list.place = "디큐브시티홀"
+        list.runtime = "180분"
+        list.time = "19:00"
+        let review = UserReview()
+        review.text = "고양이가 보고싶다 스트링 리스트에 어떻게 접근하나요 ."
+        review.image.append(objectsIn: ["jpg", "jpg"])
+        list.userReview.append(review)
+        list.userReview.append(review)
+        let repository = UserPlayRepository()
+        repository.createPlay(list)
+        let fetch = repository.fetch()
+        print(fetch)
+        
+        print("Realm:",repository.localRealm.configuration.fileURL!)
+        
+        let fetchFilter = repository.fetchFilter("율")
+        print("###율 검색결과", fetchFilter)
+        
+        let fetchFilter2 = repository.fetchFilter("에릭")
+        print("###에릭 검색결과", fetchFilter2)
+        
+        let fetchFilter3 = repository.fetchFilter("이호영")
+        print("###이호영 검색결과", fetchFilter3)
     }
 }

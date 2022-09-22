1559e4f9e316236531650cab0a60a59ee03c350a
Doy Kim <kimdee.dev@gmail.com>
2022-09-22 11:28:17 +0900

    [Design] - #18 검색결과 마지막에 붙는 사용자입력 유도 셀의 위아래 inset 조정

--- YeonMuLog/Presentations/SearchPlay/AddPlayInfoTableViewCell.swift
+++ YeonMuLog/Presentations/SearchPlay/AddPlayInfoTableViewCell.swift
@@ -44,12 +44,13 @@ final class AddPlayInfoTableViewCell: UITableViewCell {
     func setConstraints() {
         guideLabel.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
-            make.top.equalToSuperview().inset(12)
+            make.top.equalToSuperview().inset(20)
         }
         characterImageView.snp.makeConstraints { make in
             make.centerX.equalTo(guideLabel)
             make.top.equalTo(guideLabel.snp.bottom).offset(4)
             make.width.height.equalTo(80)
+            make.bottom.equalToSuperview().inset(20)
             
         }
     }

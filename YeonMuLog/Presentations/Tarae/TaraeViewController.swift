ed4425336c9072e6ebef201164a962cd0959b148
Doy Kim <kimdee.dev@gmail.com>
2022-09-23 09:20:27 +0900

    [Design] - #19 관람기록 없을때 보여지는 셀 구성

--- YeonMuLog/Presentations/Tarae/TaraeViewController.swift
+++ YeonMuLog/Presentations/Tarae/TaraeViewController.swift
@@ -34,8 +34,31 @@ class TaraeViewController: BaseViewController {
         
         let searchAndAdd = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(searchAndAddButtonTapped))
         searchAndAdd.tintColor = .black
+        navigationItem.backButtonTitle = ""
         
         navigationItem.rightBarButtonItem = searchAndAdd
     }
+    
+    override func configure() {
+        mainView.tableView.delegate = self
+        mainView.tableView.dataSource = self
+        
+        mainView.tableView.register(NoReviewTableViewCell.self, forCellReuseIdentifier: String(describing: NoReviewTableViewCell.self))
+    }
+}
 
+// MARK: - Table View
+
+extension TaraeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: NoReviewTableViewCell.self)) as? NoReviewTableViewCell else { return UITableViewCell() }
+        
+        return cell
+    }
 }

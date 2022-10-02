c133a0eeb27c7ca1e715ffc51308acce82e07233
Doy Kim <kimdee.dev@gmail.com>
2022-10-02 19:20:22 +0900

    [Feature] - #50 YPImagepicker로 버튼 하나에 연결, 카메라 버튼 제거

--- YeonMuLog/Presentations/AddReview/AddReviewViewController.swift
+++ YeonMuLog/Presentations/AddReview/AddReviewViewController.swift
@@ -12,7 +12,8 @@ import YPImagePicker
 class AddReviewViewController: BaseViewController {
     // MARK: - Properties
     let mainView = AddReviewView()
-    let picker = UIImagePickerController()
+    var config = YPImagePickerConfiguration()
+    lazy var picker = YPImagePicker(configuration: config)
     
     // MARK: - Lifecycle
     override func loadView() {
@@ -20,20 +21,29 @@ class AddReviewViewController: BaseViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        imagePickerConfigure()
     }
     
     // MARK: - Actions
-    @objc func addCameraButtonTapped(_ sender: UIButton) {
-        print("사진촬영")
-        picker.sourceType = .camera
-        present(picker, animated: true, completion: nil)
-    }
 
     @objc func addGalleryButtonTapped(_ sender: UIButton) {
         print("사진추가")
-        picker.sourceType = .photoLibrary
+        
+        picker.didFinishPicking { [unowned picker] items, _ in
+            if let photo = items.singlePhoto {
+                  print(photo.fromCamera) // Image source (camera or library)
+                  print(photo.image) // Final image selected by the user
+                  print(photo.originalImage) // original image selected by the user, unfiltered
+                  print(photo.modifiedImage) // Transformed image, can be nil
+                  print(photo.exifMeta) // Print exif meta data of original image.
+              }
+              picker.dismiss(animated: true, completion: nil)
+            
+          }
+        
         present(picker, animated: true, completion: nil)
+
     }
     
     @objc func addVoiceButtonTapped(_ sender: UIButton) {
@@ -46,22 +56,26 @@ class AddReviewViewController: BaseViewController {
     
     @objc func finishReviewButtonTapped(_ sender: UIButton) {
         print("리뷰작성완료")
+        dismiss(animated: true)
     }
     
     // MARK: - Helpers
+    func imagePickerConfigure() {
+        config.library.numberOfItemsInRow = 4
+        config.library.mediaType = .photo
+        config.albumName = "YeonMuLog"
+        config.library.defaultMultipleSelection = true
+    }
+    
     override func configure() {
         mainView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.1)
         
         mainView.userTextView.delegate = self
             
-        mainView.addCameraButton.addTarget(self, action: #selector(addCameraButtonTapped), for: .touchUpInside)
         mainView.addGalleryButton.addTarget(self, action: #selector(addGalleryButtonTapped), for: .touchUpInside)
         mainView.addVoiceButton.addTarget(self, action: #selector(addVoiceButtonTapped), for: .touchUpInside)
         mainView.cancelButton.addTarget(self, action: #selector(cancelButtonTapped), for: .touchUpInside)
         mainView.finishReviewButton.addTarget(self, action: #selector(finishReviewButtonTapped), for: .touchUpInside)
-        
-        picker.delegate = self
-       
     }
 }
 
@@ -75,21 +89,4 @@ extension AddReviewViewController: UITextViewDelegate {
     }
 }
 
-// MARK: - YPImagePicker
-extension AddReviewViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-    // - 사진선택, 카메라 촬영 직후에
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        
-        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            dismiss(animated: true)
-        }
-    }
-    
-    // - 취소 버튼 눌렀을 경우
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        print(#function)
-        
-        dismiss(animated: true)
-    }
-}
+

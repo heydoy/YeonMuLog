ebd738d2def059161fbf85494e92f17bea7f067f
Doy Kim <kimdee.dev@gmail.com>
2022-10-04 01:53:56 +0900

    [Design] - #72 바차트에 내용 전달(리뷰, 요일)

--- YeonMuLog/Presentations/Chart/ChartViewController.swift
+++ YeonMuLog/Presentations/Chart/ChartViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import RealmSwift
+import Charts
 
 class ChartViewController: BaseViewController {
 
@@ -64,14 +65,92 @@ extension ChartViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         if list != nil && !list.isEmpty {
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ChartTableViewCell.self)) as? ChartTableViewCell else { return UITableViewCell() }
-            
-            return cell
+            if indexPath.row == 0 {
+                // 가장 리뷰를 많이 남긴 극
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ChartTableViewCell.self)) as? ChartTableViewCell else { return UITableViewCell() }
+                
+                let dataSet = reviewBarChartDataSet()
+                let data = dataSet.0
+                let title = dataSet.1
+                let play = dataSet.2
+                cell.setData(data: data, title: title)
+                cell.barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: play)
+                cell.barChartView.xAxis.setLabelCount(play.count, force: false)
+                return cell
+            } else if indexPath.row == 1 {
+                // 가장 관극을 많이한 요일
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: ChartTableViewCell.self)) as? ChartTableViewCell else { return UITableViewCell() }
+                
+                let dataSet = dateBarChartDataSet()
+                let data = dataSet.0
+                let title = dataSet.1
+                let day = dataSet.2
+                cell.setData(data: data, title: title)
+                cell.barChartView.xAxis.valueFormatter = IndexAxisValueFormatter(values: day)
+                cell.barChartView.xAxis.setLabelCount(day.count, force: false)
+                return cell
+            } else {
+                return UITableViewCell()
+            }
             
         } else {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: String(describing: NoChartTableViewCell.self)) as? NoChartTableViewCell else { return UITableViewCell() }
             
             return cell
         }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 380
+    }
+}
+
+// 차트 데이터 설정
+extension ChartViewController {
+    func reviewBarChartDataSet() -> (BarChartDataSet, String, [String]) {
+        var play: [String] = []
+        var reviewNumber: [Double] = []
+        
+        if let list = list {
+            list.forEach {
+                play.append($0.title)
+                reviewNumber.append(Double($0.userReview.count))
+            }
+        }
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for index in 0..<play.count {
+            let dataEntry = BarChartDataEntry(x: Double(index), y: reviewNumber[index])
+            dataEntries.append(dataEntry)
+        }
+        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "리뷰 수")
+        let title = "✍️ 리뷰를 제일 많이 쓴 극은\n\(play[reviewNumber.firstIndex(of: reviewNumber.max()!)!])으로\n총 \(Int(reviewNumber.max()!))개의 리뷰를 썼습니다."
+        return (chartDataSet, title, play)
+    }
+    
+    func dateBarChartDataSet() -> (BarChartDataSet, String, [String]) {
+        let date: [String] = ["월","화","수","목","금","토"]
+        var watched: [Double] = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        
+        if let list = list {
+            list.forEach {
+                for index in 0..<date.count {
+                    if $0.date.extractDate() == date[index] {
+                        watched[index] += 1.0
+                    }
+                }
+            }
+        }
+       
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for index in 0..<date.count {
+            let dataEntry = BarChartDataEntry(x: Double(index), y: watched[index])
+            dataEntries.append(dataEntry)
+        }
+        let chartDataSet = BarChartDataSet(entries: dataEntries, label: "요일")
+        let title = "📆 극을 제일 많이 본 요일은\n\(date[watched.firstIndex(of: watched.max()!)!])요일로\n총 \(Int(watched.max()!))번 관극하였습니다."
+        return (chartDataSet, title, date)
     }
 }

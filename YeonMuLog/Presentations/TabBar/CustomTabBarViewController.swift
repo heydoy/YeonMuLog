d075ef46fe5270f0e480fb928dce29a4e4c4ef5d
Doy Kim <kimdee.dev@gmail.com>
2022-10-03 23:39:38 +0900

    [Design] - #72 탭바 연결

--- YeonMuLog/Presentations/TabBar/CustomTabBarViewController.swift
+++ YeonMuLog/Presentations/TabBar/CustomTabBarViewController.swift
@@ -28,10 +28,16 @@ class CustomTabBarViewController: UITabBarController {
     private func setTabBarController() {
         let mainVC = TaraeViewController()
         let mainRootVC = UINavigationController(rootViewController: mainVC)
-        mainRootVC.tabBarItem = UITabBarItem(title: "메인",
+        mainRootVC.tabBarItem = UITabBarItem(title: "mainTabName".localized,
                                          image: UIImage(systemName: "ticket"),
                                          selectedImage: UIImage(systemName: "ticket.fill"))
         
-        setViewControllers([mainRootVC], animated: true)
+        let chartVC = ChartViewController()
+        let chartRootVC = UINavigationController(rootViewController: chartVC)
+        chartRootVC.tabBarItem = UITabBarItem(title: "chartTabName".localized,
+                                              image: UIImage(systemName: "chart.pie"),
+                                              selectedImage: UIImage(systemName: "chart.pie.fill"))
+        
+        setViewControllers([mainRootVC, chartRootVC], animated: true)
     }
 }

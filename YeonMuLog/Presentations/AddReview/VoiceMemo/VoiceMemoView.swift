b0277511236f840a635b1fec2eb15855b4502155
Doy Kim <kimdee.dev@gmail.com>
2022-10-03 11:22:33 +0900

    [Design] - #54 레코딩타임 레이블 제거 나중에 기능 구현 시 추가할 예정

--- YeonMuLog/Presentations/AddReview/VoiceMemo/VoiceMemoView.swift
+++ YeonMuLog/Presentations/AddReview/VoiceMemo/VoiceMemoView.swift
@@ -13,13 +13,7 @@ final class VoiceMemoView: BaseView {
         $0.image = UIImage(named: "character-mic-recording")
     }
     
-    let recordTimeLabel = UILabel().then {
-        $0.font = .appleSDGothicNeo(of: .biggestTitle, weight: .bold)
-        $0.textColor = .black
-        $0.textAlignment = .center
-        $0.text = "00:00.00"
-    }
-    
+
     let playButton = UIButton().then {
         $0.setImage(UIImage(systemName: "play.fill"), for: .normal)
         $0.setPreferredSymbolConfiguration(.init(pointSize: 50), forImageIn: .normal)
@@ -49,7 +43,6 @@ final class VoiceMemoView: BaseView {
     override func setupUI() {
         backgroundColor = .white
         addSubview(characterImageView)
-        addSubview(recordTimeLabel)
         addSubview(playButton)
         addSubview(pauseButton)
         addSubview(closeButton)
@@ -72,15 +65,11 @@ final class VoiceMemoView: BaseView {
             make.centerX.equalToSuperview()
             make.centerY.equalToSuperview().multipliedBy(0.8)
         }
-        recordTimeLabel.snp.makeConstraints { make in
-            make.centerX.equalToSuperview()
-            make.top.equalTo(characterImageView.snp.bottom).offset(8)
-        }
-        
+
         pauseButton.snp.makeConstraints { make in
             make.width.height.equalTo(60)
             make.centerX.equalToSuperview()
-            make.top.equalTo(recordTimeLabel.snp.bottom).offset(16)
+            make.top.equalTo(characterImageView.snp.bottom).offset(16)
         }
         playButton.snp.makeConstraints { make in
             make.centerX.top.width.height.equalTo(pauseButton)

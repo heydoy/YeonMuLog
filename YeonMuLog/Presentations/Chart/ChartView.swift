e8b19c38ad7a58625a07a88d6d9fe95a183a9477
Doy Kim <kimdee.dev@gmail.com>
2022-10-03 23:39:19 +0900

    [Design] - #72 차트 View, VC 파일 구성

---
+++ YeonMuLog/Presentations/Chart/ChartView.swift
@@ -0,0 +1,20 @@
+//
+//  ChartView.swift
+//  YeonMuLog
+//
+//  Created by Doy Kim on 2022/10/03.
+//
+
+import UIKit
+
+class ChartView: BaseView {
+    
+
+    override func setupUI() {
+        backgroundColor = .white
+    }
+    
+    override func setConstraints() {
+        
+    }
+}
---
+++ YeonMuLog/Presentations/Chart/ChartViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ChartViewController.swift
+//  YeonMuLog
+//
+//  Created by Doy Kim on 2022/10/03.
+//
+
+import UIKit
+import RealmSwift
+
+class ChartViewController: BaseViewController {
+
+    // MARK: - Properties
+    let mainView = ChartView()
+    let repository = UserPlayRepository.shared
+    var list: Results<UserPlayInfo>!
+    
+    // MARK: - Lifecycle
+    override func loadView() {
+        view = mainView
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tabBarController?.tabBar.isHidden = false
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if repository.fetch() != list {
+            list = repository.fetch()
+       }
+    }
+    
+    // MARK: - Actions
+    
+    
+    // MARK: - Helpers
+    override func setNavigationBar() {
+        navigationItem.title = "통계"
+    }
+    override func configure() {
+        
+    }
+    
+    
+
+}

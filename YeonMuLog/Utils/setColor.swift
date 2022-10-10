547106cdb3b92354b4f85690954a24ebae339cb8
Doy Kim <kimdee.dev@gmail.com>
2022-10-10 23:23:26 +0900

    [Chore] - #81 기존 로고 보라색컬러를 UIColor 익스텐션으로 추가

---
+++ YeonMuLog/Utils/setColor.swift
@@ -0,0 +1,20 @@
+//
+//  setColor.swift
+//  YeonMuLog
+//
+//  Created by Doy Kim on 2022/10/10.
+//
+
+import UIKit
+
+extension UIColor {
+    struct CustomColor {
+        static let purple30 = #colorLiteral(red: 0.9764705882, green: 0.9529411765, blue: 0.9921568627, alpha: 1)
+        static let purple100 = #colorLiteral(red: 0.7803921569, green: 0.6117647059, blue: 1, alpha: 1)
+        static let purple200 = #colorLiteral(red: 0.7098039216, green: 0.4862745098, blue: 1, alpha: 1)
+        static let purple500 = #colorLiteral(red: 0.4823529412, green: 0.262745098, blue: 0.7607843137, alpha: 1)
+        static let purple800 = #colorLiteral(red: 0.2745098039, green: 0.1176470588, blue: 0.4745098039, alpha: 1)
+        
+        // #colorLiteral(red:  , green: , blue: , alpha: )
+    }
+}

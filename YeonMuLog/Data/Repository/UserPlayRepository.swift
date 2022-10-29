3a93ad52bed2a97d510b90f86597c0deffd359a3
Doy Kim <kimdee.dev@gmail.com>
2022-10-29 18:16:35 +0900

    [Feat] #108 레포지토리 패턴에 날짜순, 가나다순, 리뷰유무여부로 정렬하여 가져오는 메서드 추가

--- YeonMuLog/Data/Repository/UserPlayRepository.swift
+++ YeonMuLog/Data/Repository/UserPlayRepository.swift
@@ -8,12 +8,19 @@
 import Foundation
 import RealmSwift
 
+enum SortOption: String {
+    case date = "date"
+    case userReview = "userReview"
+    case title = "title"
+}
+
 fileprivate protocol UserPlayRepositoryType: AnyObject {
     func fetch() -> Results<UserPlayInfo>
+    func fetchWith(sort: SortOption, option: Bool) -> Results<UserPlayInfo>
+    func fetchFilter(_ query: String) -> Results<UserPlayInfo>
     func createPlay(_ item: UserPlayInfo)
     func updatePlay(_ item: UserPlayInfo)
     func deletePlay(_ item: UserPlayInfo)
-    
     func updateReview(_ item: UserPlayInfo)
 }
 
@@ -23,7 +30,22 @@ class UserPlayRepository {
     let localRealm = try! Realm()
     
     func fetch() -> Results<UserPlayInfo> {
-        return localRealm.objects(UserPlayInfo.self).sorted(byKeyPath: "date", ascending: false) // 항상 최신순
+        return localRealm.objects(UserPlayInfo.self).sorted(byKeyPath: "date", ascending: false) // 최신 순서
+    }
+    
+    func fetchWith(sort: SortOption, option: Bool) -> Results<UserPlayInfo> {
+        switch sort {
+        case .date, .title :
+            // 날짜, 또는 가나다 순
+            return localRealm.objects(UserPlayInfo.self).sorted(byKeyPath: sort.rawValue, ascending: option)
+        
+        case .userReview :
+            // 리뷰 있을 경우만
+            let predicate = NSPredicate(format: "#userReview.@count > 0")
+            return localRealm.objects(UserPlayInfo.self).filter(predicate)
+            
+        }
+        
     }
     
     func fetchFilter(_ query: String) -> Results<UserPlayInfo> {
@@ -33,9 +55,9 @@ class UserPlayRepository {
             $0.title.contains(query, options: .caseInsensitive) ||
             $0.place.contains(query, options: .caseInsensitive) ||
             $0.userReview.text.contains(query, options: .caseInsensitive) ||
-            $0.casts == query  // 캐스트 이름이 일치하면 검색가능 (일부로 할 경우 String 타입의 프로퍼티가 있는 오브젝트 클래스를 만들어야함) 
+            $0.casts == query  // 캐스트 이름이 일치하면 검색가능 (일부로 할 경우 String 타입의 프로퍼티가 있는 오브젝트 클래스를 만들어야함)
         }
-
+        
         return results
     }
     
